8bbf533f70092e77b6c68a271901f2b341139934
jarod <jarod@jarod.com>
2024-02-04 15:40:38 +1100

    Remove the extern C stuff from the cpp code

--- example.cpp
+++ example.cpp
@@ -9,21 +9,3 @@ int MyClass::getValue() const {
 void MyClass::setValue(int newValue) {
     value = newValue;
 }
-
-extern "C" {
-    MyClass* create_object(int initialValue) {
-        return new MyClass(initialValue);
-    }
-
-    int get_value(MyClass* obj) {
-        return obj->getValue();
-    }
-
-    void set_value(MyClass* obj, int newValue) {
-        obj->setValue(newValue);
-    }
-
-    void delete_object(MyClass* obj) {
-        delete obj;
-    }
-}
