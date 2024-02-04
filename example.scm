9ada31c2484a60b249edd4ede7565a69326b8d55
jarod <jarod@jarod.com>
2024-02-04 16:03:03 +1100

    Formatting

--- example.scm
+++ example.scm
@@ -1,17 +1,18 @@
-(load-extension "./libexample.so" "SWIG_init")
+(load-extension "./libexample" "SWIG_init")
 
-; Example of using the C++ code from Scheme
-(display "Creating C++ object from Scheme: ")
+(display " -- Creating C++ object from Scheme -- ")
 (let* ((obj (new-MyClass 42))
        (initial-value (MyClass-getValue obj)))
+  (newline)
   (display "Initial value: ")
   (display initial-value)
   (newline)
 
-  (display "Setting new value from Scheme: ")
+  (display " -- Setting new value from Scheme -- ")
+  (newline)
   (MyClass-setValue obj 99)
   (display "New value: ")
   (display (MyClass-getValue obj))
   (newline)
 
-  (delete-MyClass obj)) ; Clean up the C++ object
+  (delete-MyClass obj))
